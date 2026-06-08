aaea16e497827c18471759e3fdc55af87f722f8a
Akriti Raj <rajakriti24@gmail.com>
2026-06-09 01:14:51 +0530

    Add draw_line function

--- main.c
+++ main.c
@@ -101,12 +101,28 @@ void draw_rectangle(int x, int y, int width, int height)
     for(i = y; i < y + height; i++)
         put_dot(i, x + width - 1);
 }
+void draw_line(int x1, int y1, int x2, int y2)
+{
+    int i;
 
+    if(y1 == y2) // horizontal line
+    {
+        for(i = x1; i <= x2; i++)
+            put_dot(y1, i);
+    }
+
+    else if(x1 == x2) // vertical line
+    {
+        for(i = y1; i <= y2; i++)
+            put_dot(i, x1);
+    }
+}
 int main(void)
 
 {
     clear_canvas();
     draw_rectangle(10, 5, 20, 8);
+    draw_line(5, 15, 25, 15);
     show_canvas();
 
     return 0;
