7541e5b8c3535720d7cf208ac1a407fe90cd07e5
Akriti Raj <rajakriti24@gmail.com>
2026-06-08 23:53:49 +0530

    second commit

--- main.c
+++ main.c
@@ -81,12 +81,32 @@ void remove_dot(int row, int col)
     if (row >= 0 && row < ROWS && col >= 0 && col < COLS)
         canvas[row][col] = BLANK;
 }
+void draw_rectangle(int x, int y, int width, int height)
+{
+    int i;
 
+    // top side
+    for(i = x; i < x + width; i++)
+        put_dot(y, i);
+
+    // bottom side
+    for(i = x; i < x + width; i++)
+        put_dot(y + height - 1, i);
+
+    // left side
+    for(i = y; i < y + height; i++)
+        put_dot(i, x);
+
+    // right side
+    for(i = y; i < y + height; i++)
+        put_dot(i, x + width - 1);
+}
 
 int main(void)
 
 {
     clear_canvas();
+    draw_rectangle(10, 5, 20, 8);
     show_canvas();
 
     return 0;
